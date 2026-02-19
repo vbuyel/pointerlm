3d1f20b3f1d42f3d4ada74884642e94b526600e0
vbuyel <vladosbuyel@gmail.com>
2026-02-19 22:46:38 +0300

    added https in login

--- content/static/js/login.js
+++ content/static/js/login.js
@@ -30,7 +30,7 @@ form.addEventListener('submit', async (e) => {
         return;
     }
 
-    const response = await fetch(`http://127.0.0.1:8000/user/login`, {
+    const response = await fetch(`https://transudative-santo-hurtless.ngrok-free.dev/user/login`, {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams({
